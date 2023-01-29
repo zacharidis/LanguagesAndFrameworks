7d42e65b50ca625c39a7a5ea903b5e01d3906e16
Georgios Zacharidis <zageo@outlook.com>
2023-01-29 20:32:17 +0200

    Improved model for framework

--- Model/Domain/Framework.cs
+++ Model/Domain/Framework.cs
@@ -2,5 +2,21 @@
 {
     public class Framework
     {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string CreatorsName { get; set; }
+
+
+        // ref to Language model 
+
+        public Guid LanguageID { get; set; }
+
+        public Guid ProjectsID { get; set; }
+
+
+        public Framework() { }
+
     }
 }
