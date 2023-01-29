5330f4c7be1a42d0bd6bcabb1f57d7fd0fb43f49
Georgios Zacharidis <zageo@outlook.com>
2023-01-29 20:34:24 +0200

    Blueprint for project class added

---
+++ Model/Domain/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace LanguagesAndFrameworks.Model.Domain
+{
+    public class Project
+    {
+        public Guid ID { get; set; }
+        public String Type { get; set; }
+
+        public String Description { get; set; }
+
+
+
+    }
+}
