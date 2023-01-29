1fe3f7f11ef72e011035e4628b9d7c68dca04afc
Georgios Zacharidis <zageo@outlook.com>
2023-01-29 20:50:28 +0200

    Added properties inside LanguageDbContext

--- Data/LanguageDbContext.cs
+++ Data/LanguageDbContext.cs
@@ -4,6 +4,15 @@ namespace LanguagesAndFrameworks.Data
 {
     public class LanguageDbContext : DbContext
     {
+        public LanguageDbContext(DbContextOptions<LanguageDbContext> options) :base(options)
+        {
+            
+        }
 
+
+        public DbSet<Model.Domain.Language> Languages { get; set; }
+        public DbSet<Model.Domain.Framework> Frameworks { get; set; }
+
+        public DbSet<Model.Domain.Project> Projects { get; set; }
     }
 }
